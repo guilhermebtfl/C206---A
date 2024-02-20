e228a4af0a9cbb8f6a1169bd5ce1ad4c199dd15c
guilhermebtfl <160641930+guilhermebtfl@users.noreply.github.com>
2024-02-20 14:38:59 -0300

    Exercício Trailler do Zé

---
+++ TraillerLanche_ex1/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int lanche1 = 10;
+        int lanche2 = 4;
+        int lanche3 = 2;
+
+        int soma =(lanche1 + lanche2 + lanche3);
+        float media = soma/3.0f;
+
+        System.out.println("O total de lanches foi: "+soma);
+        System.out.println("A media de lanches foi: "+media);
+
+    }
+}
